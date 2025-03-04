9ff05f4506ecd2a945f176fc927fd1c36b3712f3
Edouard Dieppois <Dieppoisedouard@gmail.com>
2025-03-04 16:10:11 +0100

    Added edit/update method + partial view

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,19 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.all
   end
+
+  def edit
+    @beer = Beer.find(params[:id])
+  end
+  
+  def update
+    @beer.update(beer_params)
+    redirect_to beers_path
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :degree, :centiliter, :unit_price, :conditionnement, :beer_type)
+  end
 end
