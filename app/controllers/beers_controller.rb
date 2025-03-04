20c12339e6efe68817e543369070703a55725ccd
valentin le pape <valentinlepape@gmail.com>
2025-03-04 16:20:25 +0100

    en cours 2

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,24 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.all
   end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+    @beer.user_id = current_user.id
+    if @beer.save
+      redirect_to beer_path(@beer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def beer_params
+    params.require(:beer).permit(:name, :degree, :centiliter, :unit_price, :conditionnement, :beer_type)
+  end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,5 @@
 class Beer < ApplicationRecord
-  belongs_to :users_id
+  belongs_to :user
 
   validates :name, :degree, :centiliter, :unit_price, :conditionnement, :beer_type, presence: true
 end
