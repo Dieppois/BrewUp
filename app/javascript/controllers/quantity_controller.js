e2824ff376d682608506d86430bbab25ea8dea5a
valentin le pape <valentinlepape@gmail.com>
2025-03-07 13:09:27 +0100

    J'ai ajouté un peu de dynamisme aux prix

--- app/javascript/controllers/quantity_controller.js
+++ app/javascript/controllers/quantity_controller.js
@@ -6,20 +6,22 @@ export default class extends Controller {
   static targets = ["quantity"];
 
   increment() {
-    this.quantityTarget.value = Number(this.quantityTarget.value) + 1;
+    let currentValue = parseInt(this.quantityTarget.value, 10) || 0;
+    this.quantityTarget.value = currentValue + 1;
     this.updateHiddenQuantity();
+    this.dispatch("update", { detail: { quantity: this.quantityTarget.value } });
   }
-
   decrement() {
-    if (this.quantityTarget.value > 1) {
-      this.quantityTarget.value = Number(this.quantityTarget.value) - 1;
+    let currentValue = parseInt(this.quantityTarget.value, 10) || 0;
+    if (currentValue > 1) { // Prevent going below 1
+      this.quantityTarget.value = currentValue - 1;
       this.updateHiddenQuantity();
+      this.dispatch("update", { detail: { quantity: this.quantityTarget.value } });
     }
-  }
 
   // addToCart() {
   //   const quantity = this.quantityTarget.value;
   //   alert(`Ajouté au panier : ${quantity} x ${this.element.dataset.name}`);
   // }
   }
-
+}
